66b1d8d6c9d9e9383b7997d61779cb1f951875db
MatiasSolisBello <matias_solis11@hotmail.com>
2020-05-11 13:56:20 -0400

    Model Done

--- usuario/admin.py
+++ usuario/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from usuario.models import Rol, Estado, Usuario, Mascota
 
 # Register your models here.
+class MascotaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'raza', 'estado')
+    #filter_horizontal = ('estado', )
+
+class EstadoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'estado')
+
+class RolAdmin(admin.ModelAdmin):
+    list_display = ('id', 'rol')
+
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'rut', 'correo', 'rol')
+
+admin.site.register(Rol, RolAdmin)
+admin.site.register(Estado, EstadoAdmin)
+admin.site.register(Usuario, UsuarioAdmin)
+admin.site.register(Mascota, MascotaAdmin)
--- usuario/models.py
+++ usuario/models.py
@@ -1,24 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class Rol(models.Model):
+    id = models.CharField(max_length=200, primary_key=True)
+    rol = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.rol
+
 class Usuario(models.Model):
     rut = models.CharField(max_length=200)
     nombre = models.CharField(max_length=200)
     correo = models.EmailField()
     password = models.CharField(max_length=200)
-    rol = [
-        ('ADM', 'Admin'),
-        ('ADO', 'Adoptante'),
-    ]
+    rol = models.ForeignKey('Rol', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Estado(models.Model):
+    id = models.CharField(max_length=200, primary_key=True)
+    estado = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.estado
+
 
 class Mascota(models.Model):
     foto = models.ImageField(upload_to = 'tmp')
     nombre = models.CharField(max_length=200)
     raza = models.CharField(max_length=200)
     descripcion = models.TextField()
-    estado = [
-        ('R', 'Rescatado'),
-        ('D', 'Disponible'),
-        ('A', 'Adoptado'),
-    ]
-    rut = models.ManyToManyField(Usuario)
+    estado = models.ForeignKey('Estado', on_delete=models.CASCADE)
+    rut = models.ForeignKey('Usuario', on_delete=models.CASCADE)
